5a2ae1647bf9b8f9f865a37cacafefa4810b9cc6
3Nakajugo <edinahmargarita@gmail.com>
2021-04-12 16:44:46 +0300

    modify api

--- App/movie_model.py
+++ App/movie_model.py
@@ -1,26 +1,32 @@
+from datetime import date
+
 movies = [
     {
         "id": 1,
         "title": "hello",
-        "description": "mlcmlodaflamclxzmcopd"
-    },
-    {
-        "id": 2,
-        "title": "hello",
-        "description": "mlcmlodaflamclxzmcopd"
+        "description": "mlcmlodaflamclxzmcopd",
+        "release_date": "16/09/2019",
+        "director": "edna"
+
     }
 ]
 
 
 class Movie:
-    def __init__(self, title, description):
+    def __init__(self, title, description, release_date, language, director):
         self.id = len(movies)+1
         self.title = title
         self.description = description
+        self.release_date = date.today()
+        self.language = language
+        self.director = director
 
     def to_json(self):
         return {
             "id": self.id,
             "title": self.title,
-            "description": self.description
+            "description": self.description,
+            "release_date": self.release_date,
+            "language": self.language,
+            "director": self.director
         }
--- App/movie_view.py
+++ App/movie_view.py
@@ -5,8 +5,10 @@ from .movie_model import movies, Movie
 
 @app.route("/api/v1/movies", methods=['GET'])
 def get_all_movies():
-    all_movies = jsonify(movies)
-    return all_movies, 200
+    if len(movies) > 0:
+        all_movies = jsonify(movies)
+        return all_movies, 200
+    return jsonify({"message": "no movies to display"})
 
 
 @app.route("/api/v1/movies/<int:id>", methods=['GET'])
@@ -21,9 +23,13 @@ def get_movie_by_id(id):
 def create_movie():
     request_data = request.get_json()
     title = request_data['title']
-    description = request_data["description"]
+    description = request_data['description']
+    release_date = request_data['release_date']
+    language = request_data['language']
+    director = request_data['director']
 
-    movie = Movie(title=title, description=description).to_json()
+    movie = Movie(title=title, description=description, release_date=release_date,
+                  language=language, director=director).to_json()
     movies.append(movie)
     return jsonify({"message": "movie created successful",
                     "new movie": movie
@@ -35,5 +41,22 @@ def delete_movie_by_id(id):
     for movie in movies:
         if movie['id'] == id:
             movies.remove(movie)
-        return jsonify({"message": " movie deleted successfully", "all_movies": movies}), 200
-    return jsonify({'message': 'not found'}), 404
+            return jsonify({"message": " movie deleted successfully", "all_movies": movies}), 200
+        return jsonify({'error': 'movie not found'}), 404
+
+
+@app.route("/api/v1/movies/<int:id>", methods=['PUT'])
+def update_movie_by_id(id):
+    request_data = request.get_json()
+
+    for movie in movies:
+        if movie['id'] == id:
+            title = request_data['title']
+            description = request_data["description"]
+            release_date = request_data['release_date']
+            language = request_data['language']
+            director = request_data['director']
+            movie.update(title=title, description=description, release_date=release_date,
+                         language=language, director=director)
+            return jsonify({"message": " movie updated successfully", "all_movies": movie}), 200
+        return jsonify({'error': 'not found'}), 404
